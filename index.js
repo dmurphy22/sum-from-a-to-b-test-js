22b44accf76da4d226c667689e9ac204fc8a5f7f
Darragh Murphy <dmurphy@servuo.com>
2023-05-05 18:44:40 -0700

    Implement sum recursively.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 
-function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
-}
+const sum = function(fromN, toN) {
+  if (fromN === toN) {
+    return fromN;
+  }
+  return fromN + sum(fromN + 1, toN);
 
+};
+sum(3,7);
 module.exports = sum;
